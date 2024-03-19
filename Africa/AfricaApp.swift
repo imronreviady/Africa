2a038dd885bdcdc1bd10efb10ea3660da1dcd218
Imron Reviady <imronreviady2@gmail.com>
2024-03-19 13:45:41 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Imron Reviady on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
